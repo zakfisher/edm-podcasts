cd3572f47224a0e3b45dc159b588491a29cc7e6b
Dan Sim <dsim@au.westfield.com>
2014-10-17 13:32:44 -0700

    new map display ideas, quick search mockup

--- app/modules/home/index.js
+++ app/modules/home/index.js
@@ -4,6 +4,26 @@ module.exports = angular.module('wayfindingMap', [])
 
 .controller('KioskHome', function ($scope, $famous) {
   $scope.message = "Wayfinding Prototype Home";
+
+  $scope.search = {
+    query: '',
+    results: []
+  };
+
+  $scope.$watch('search.query', function (a, b) {
+    var r;
+    if (!a) {
+      r = [];
+    } else if (a && a !== b) {
+      r = JMap.getSearchByQuery(a);
+      console.log(a, r);
+    }
+    $scope.search.results = r;
+
+  });
+
+
+  console.log('JMap', JMap);
   $scope.bound = {
     tilt: 0
   };
