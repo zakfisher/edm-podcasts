0c0ed6b9f8e8e6d9f2ece5f150756656a3c058de
Dan Sim <dsim@au.westfield.com>
2014-11-17 15:04:04 -0800

    enable shopping directory

--- app/modules/dining/index.js
+++ app/modules/dining/index.js
@@ -2,8 +2,24 @@
 
 module.exports = angular.module('Dining', [])
 
-.controller('Dining', function ($scope, $famous) {
-  console.log("Dining Module");
+.controller('Dining', function ($scope, $famous, $http) {
+  //Urls for directory
+  $scope.storesUrl = "http://www.westfield.com.au/api/store/master/stores.json?centre_id=valleyfair&country=us&per_page=all";
+  $scope.categoriesUrl = "http://www.westfield.com.au/api/category/master/categories/fooddrink.json?centre_id=valleyfair&country=us&per_page=all";
+
+  //Get All Categories
+  $http({
+    method: 'GET',
+    url: $scope.categoriesUrl
+  }).success(function (r) {
+    console.log('categories', r);
+    $scope.categories = r.children;
+    $scope.categories.unshift({
+      name: "All Dining Choices",
+      code: "fooddrink"
+    });
+  });
+
 })
 
 //Routes / States
