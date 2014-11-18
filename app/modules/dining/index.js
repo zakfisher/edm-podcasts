25c9349da18f5798a5fae17aadc88536e387172a
Dan Sim <dsim@au.westfield.com>
2014-11-18 10:53:12 -0800

    add dining bg, directory title control, Category Service getChildCategoriesOf()

--- app/modules/dining/index.js
+++ app/modules/dining/index.js
@@ -2,25 +2,18 @@
 
 module.exports = angular.module('Dining', [])
 
-.controller('Dining', function ($scope, $famous, $http) {
-  //Urls for directory
-  $scope.storesUrl = "http://www.westfield.com.au/api/store/master/stores.json?centre_id=valleyfair&country=us&per_page=all";
-  $scope.categoriesUrl = "http://www.westfield.com.au/api/category/master/store-categories/valleyfair.json";
+.controller('Dining', function ($scope, $famous, $http, CategoryService) {
+  $scope.directoryTitle = "Dining";
 
-  //Get All Categories
-  $http({
-    method: 'GET',
-    cache: true,
-    url: $scope.categoriesUrl
-  }).success(function (r) {
-    console.log('\n\ncategories', r);
-    $scope.categories = r[0].children;
-    $scope.categories.unshift({
-      name: "All Dining Choices",
-      code: "fooddrink"
-    });
-    $scope.filterCategory = "fooddrink";
+  // @todo check how hungry this is on memory. Looks ripe for optimisation.
+  $scope.categories = CategoryService.getChildCategoriesOf('fooddrink');
+  $scope.categories.unshift({
+    name: "All Dining Choices",
+    code: "fooddrink"
   });
+
+  $scope.currentCategoryName = 'All Dining Choices';
+  $scope.filterCategory = 'fooddrink';
 
 })
 
