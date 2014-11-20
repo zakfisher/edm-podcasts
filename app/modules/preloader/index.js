c0a7aba2eb31ac3c8ac6955fadffe57af462ee2c
Dan Sim <dsim@au.westfield.com>
2014-11-20 09:34:59 -0800

    go to kiosk map on load

--- app/modules/preloader/index.js
+++ app/modules/preloader/index.js
@@ -41,7 +41,7 @@ module.exports = angular.module('LoadingScreen', [])
   }, 1);
 
   Preloader.whenFinished().then(function () {
-    $state.go('largescreen');
+    $state.go('kioskmap');
     KioskMenu.show();
   });
 })
