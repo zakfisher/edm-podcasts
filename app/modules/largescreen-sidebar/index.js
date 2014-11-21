69c5022407f6cc94f47b551dde1b06b09abf1720
Zak Fisher <zak.fisher@gmail.com>
2014-11-20 16:39:14 -0800

    sidebar menu toggling

--- app/modules/largescreen-sidebar/index.js
+++ app/modules/largescreen-sidebar/index.js
@@ -5,38 +5,112 @@ module.exports = angular.module('LargescreenSidebar', [])
     Transitionable = $famous['famous/transitions/Transitionable'],
     Easing = $famous['famous/transitions/Easing'];
 
-  sidebar.width = 350;
-  sidebar.openPosition = [0, 0, 0];
-  sidebar.closePosition = [(sidebar.menuWidth * -1), 0, 0];
-  sidebar.position = sidebar.openPosition;
-  
-  sidebar.sizeTransitionable = new Transitionable([sidebar.width, undefined]);
+  sidebar.size = [309, undefined];
 
-  // sidebar.show = function () {
-  //   sidebar.visible = true;
-  // };
+  var openPosition = [0, 0, 0];
+  var closePosition = [-254, 0, 0];
+
+  sidebar.positionTransitionable = new Transitionable(closePosition);
+
+  sidebar.getContentHeight = function (floorCount, categoryCount) {
+    var contentHeight = 0;
+    var listCount = 2;
+    var listTitleHeight = 25;
+    var listItemHeight = 46;
+    var listMarginHeight = 32;
+    // Add List Title
+    contentHeight += (listCount * listTitleHeight);
+    // Add List Margins
+    contentHeight += (listCount * listMarginHeight);
+    // Add Category List Items
+    contentHeight += (categoryCount * listItemHeight);
+    // Add Floor List Items
+    contentHeight += (floorCount * listItemHeight);
+    return contentHeight;
+  };
+
+  sidebar.toggle = function() {
+    if (sidebar.active) {
+      sidebar.contract();
+    }
+    else {
+      sidebar.expand();
+    }
+  };
+
+  sidebar.expand = function () {
+    console.log('expand sidebar');
+    sidebar.active = true;
+    sidebar.positionTransitionable.set(
+      openPosition, {
+        duration: 200,
+        curve: Easing.inSine
+      }
+    );
+  };
+
+  sidebar.contract = function () {
+    console.log('contract sidebar');
+    sidebar.active = false;
+    sidebar.positionTransitionable.set(
+      closePosition, {
+        duration: 200,
+        curve: Easing.inSine
+      }
+    );
+  };
+
+  sidebar.open = function () {
+    console.log('open sidebar');
+    sidebar.active = true;
+    sidebar.positionTransitionable.set(
+      openPosition, 
+      { duration: 0 }
+    );
+  };
+
+  sidebar.close = function () {
+    console.log('close sidebar');
+    sidebar.active = false;
+    sidebar.positionTransitionable.set(
+      closePosition, 
+      { duration: 0 }
+    );
+  };
 
   return sidebar;
 })
-
 
 .directive('largescreenSidebar', function () {
   return {
     restrict: 'E',
     template: require('./largescreen-sidebar.html'),
-    controller: function ($scope, LargescreenSidebar, config) {
+    controller: function ($scope, LargescreenSidebar, CategoryService) {
       $scope.sidebar = LargescreenSidebar;
-      
+      $scope.categories = CategoryService.getCategories();
+      $scope.floors = [
+        { level: 2 },
+        { level: 1 }
+      ];
+
       $scope.menu = {
-        size: [300, undefined]
+        size: [274, undefined]
+      };
+
+      $scope.content = {
+        alignment: [0.5, 0.5],
+        origin: [0.5, 0.5],
+        size: [184, $scope.sidebar.getContentHeight($scope.floors.length, $scope.categories.length)]
       };
 
       $scope.btn = {
         alignment: [1, 0.5],
         origin: [0.5, 0],
         rotation: Math.PI / 2,
-        size: [100, 100]
+        size: [160, 45]
       };
+
+      $scope.sidebar.close();
 
     }
   };
