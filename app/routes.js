16d37c2cfaf21bc980f21704418e3b5e9946467e
Zak Fisher <zak.fisher@gmail.com>
2014-12-12 10:45:52 -0800

    now using forecast.io via node server

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,19 @@
 exports.index = function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 };
+
+exports.weather = function (req, res) {
+	var Forecast = require('forecast.io');
+	var options = {
+	  // APIKey: process.env.FORECAST_API_KEY
+	  APIKey: '9e39cf0f631c2bd05927bd364942a3e6'
+	},
+	forecast = new Forecast(options);
+	// Valley Fair Coordinates
+	var latitude = 37.325087;
+    var longitude = -121.946273;
+	forecast.get(latitude, longitude, function (err, response, data) {
+	  if (err) throw err;
+	  res.json(data);
+	});
+};
--- app/server.js
+++ app/server.js
@@ -7,12 +7,18 @@ var app = express();
 var routes = require('./routes');
 
 app.set('port', (process.env.PORT || 9292));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+
+//Fall back to angular routes
+app.get('/', routes.index);
+app.get('/weather', routes.weather);
+// app.get('*', routes.index);
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-//Fall back to angular routes
-app.get('/', routes.index);
-// app.get('*', routes.index);
