45254c19559f5d9edb9e935df2b8e0d8a1f637bc
Dan Sim <dsim@au.westfield.com>
2014-12-16 15:56:01 +1100

    remove example spec

--- app/modules/centre-service/centre-service-spec.js
+++ app/modules/centre-service/centre-service-spec.js
@@ -1,11 +1,5 @@
 describe('centre service', function () {
-
-  beforeEach(function () {
-    browser.get('http://www.angularjs.org');
-  });
-
   it('should get the centre info', function () {
-
     // var service = angular.injector(['CentreService']);
     // console.log(service);
   });
