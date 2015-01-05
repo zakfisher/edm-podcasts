65a85add432d3bb6145485c3da4b2b529a5fed71
Dan Sim <dsim@au.westfield.com>
2015-01-05 11:54:10 +1100

    more accurate startup log

--- app/server.js
+++ app/server.js
@@ -20,5 +20,5 @@ app.get('/weather', routes.weather);
 // app.get('*', routes.index);
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("App server is running at localhost:" + app.get('port'));
 });
